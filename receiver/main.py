123403d3f4d096d1ce28e46e84a616edf3447f0d
darkvictor13 <01victoralmeida@gmail.com>
2022-06-21 11:26:21 -0300

    artigo base

--- receiver/main.py
+++ receiver/main.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python
+import time
+import serial
+
 def main():
-    print("Hello World!")
+    ser = serial.Serial (
+        port='/dev/ttyS0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    while True:
+        x=ser.readline()
+        print(x.decode('ascii'), end='')
 
 if __name__ == "__main__":
     main()
--- sender/main.py
+++ sender/main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python
+import time
+import serial
+
 def main():
-    print("Hello World!")
+    ser = serial.Serial (
+        port='/dev/ttyS0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+
+    print('Enviando dados via lora:')
+    ser.write(bytearray([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]))
 
 if __name__ == "__main__":
     main()
